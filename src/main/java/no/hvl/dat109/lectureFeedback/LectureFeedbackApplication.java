0181e552a38b0f54c8354eab0c143b9ed9138b43
h676672 <676672@stud.hvl.no>
2025-03-19 16:37:14 +0100

    init commit

---
+++ src/main/java/no/hvl/dat109/lectureFeedback/LectureFeedbackApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.lectureFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureFeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/hvl/dat109/lectureFeedback/LectureFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.lectureFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
